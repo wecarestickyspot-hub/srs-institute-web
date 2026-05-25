caf7c525b99334789f97a4a79daa93b9752f2c19
wecarestickyspot <wecarestickyspot@gmail.com>
2026-05-25 16:34:57 +0530

    update sitemap.xml and

--- sitemap.js
+++ sitemap.js
@@ -2,7 +2,7 @@ export default function sitemap() {
   return [
     {
       url: 'https://srscomputerinstitute.in',
-      lastModified: new Date(),
+      lastModified: new Date(),    
       changeFrequency: 'weekly',
       priority: 1,
     },
